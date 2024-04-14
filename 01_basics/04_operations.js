7aa5f384980d9f65e988a4d5517bc307b77796f2
Vedant Kulkarni <110988642+vedantk1502@users.noreply.github.com>
2024-04-14 04:54:11 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Vedant"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(3 + 4 * 5 % 3);  // not recommended
+// console.log((3 + 4) * 5 % 3);  // recommended
+
+// console.log(true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2 //not recommended(no readibility)
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
