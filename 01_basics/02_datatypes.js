b405a17eb26623263f435edf9cb28c23a36a6e62
Vedant Kulkarni <110988642+vedantk1502@users.noreply.github.com>
2024-04-14 04:10:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3 + 3);  // code readability should be high 
+console.log("vedant"); 
+
+
+
+// datatypes 
+// number => 2 to the power of 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
