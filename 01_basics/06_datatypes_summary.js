296bb5e02a04aff1669a66176351b733813539cf
Vedant Kulkarni <110988642+vedantk1502@users.noreply.github.com>
2024-04-14 07:22:53 +0000

    summary

---
+++ 01_basics/06_datatypes_summary.js
@@ -0,0 +1,26 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefine, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+const bigNumber = 4654552154554n
+
+
+//Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj", "doga"]
