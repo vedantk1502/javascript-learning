c79a26d7eff24a22c4403cc77ee472cdfa663b97
Vedant Kulkarni <110988642+vedantk1502@users.noreply.github.com>
2024-04-15 07:10:29 +0000

    strings

--- 01_basics/07_strings.js
+++ 01_basics/07_strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 // console.log('Hello my name is ${name} and my repo count is ${repoCount}');
 
-const gameName = new String("Vedant")
+const gameName = new String("vedant-hc-com")
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -17,3 +17,16 @@ const gameName = new String("Vedant")
 
 const newString = gameName.substring(0,4)
 console.log(newString);
+const anotherString = gameName.slice(-8, 5)
+console.log(anotherString);
+
+const newStringOne ="   vedd   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://vedant.com/home%20page"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('vedant'));
+
+console.log(gameName.split('-'));
