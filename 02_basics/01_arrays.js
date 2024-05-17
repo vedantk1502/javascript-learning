dc3b5c2f597f84e6cfed900e2bdeca21ff18b3a8
Vedant Kulkarni <110988642+vedantk1502@users.noreply.github.com>
2024-05-17 17:12:11 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -25,12 +25,12 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 
 // slice, splice
-console.log("A", myArr);
+// console.log("A", myArr);
 
-const myn1 = myArr.slice(1, 3)
-console.log(myn1);
-console.log("B ", myArr);
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1);
+// console.log("B ", myArr);
 
-const myn2 = myArr.splice(1, 3)
-console.log(myn2);
-console.log("C ", myArr);
+// const myn2 = myArr.splice(1, 3)
+// console.log(myn2);
+// console.log("C ", myArr);
