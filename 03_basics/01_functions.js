87f08352e4c62b99a68ca8de395f078f6eab9a02
Vedant Kulkarni <110988642+vedantk1502@users.noreply.github.com>
2024-05-18 12:22:27 +0000

    funcs2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -47,7 +47,7 @@ function loginUserMessage2 (username = "ved"){
     }
 
 
-console.log(loginUserMessage2());
+// console.log(loginUserMessage2());
 
 
 function loginUserMessage(username){
@@ -63,3 +63,40 @@ function loginUserMessage(username){
 
 // console.log(loginUserMessage("vedant")); 
 // console.log(loginUserMessage()); 
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+function calculateCartPrice2(val1, val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice2(200, 400, 500, 2000));
+
+
+const user = {
+    username: "vedant",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.prices}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "ved",
+    prices: 499
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
