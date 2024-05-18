f39d620586438b4d105068fe8563537a965c835b
Vedant Kulkarni <110988642+vedantk1502@users.noreply.github.com>
2024-05-18 13:06:48 +0000

    scopes2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,15 +4,49 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     // var c = 30
-}
-
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
 }
 
 // console.log(a);
 // console.log(b);
-console.log(a);
+// console.log(a);
+
+function one(){
+    const username = "vedant"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true) {
+    const username = "vedant"
+    if (username === "vedant"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++
+
+// addone(5) This will not give any error because function can be called before declaration
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+// addTwo(5) This gives error because variable can't be called before declaration
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
