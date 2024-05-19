4aab7c35c0b2f0451b58e3de51a89b689a9d7e70
Vedant Kulkarni <110988642+vedantk1502@users.noreply.github.com>
2024-05-19 05:27:15 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "mar"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
