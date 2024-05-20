5971d3c923280f382aaf184bf747941d100b12a2
Vedant Kulkarni <110988642+vedantk1502@users.noreply.github.com>
2024-05-20 05:33:28 +0000

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,40 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    // console.log(item, index, arr);
+} )
+
+
+const myCoding = [
+    {
+        langName: "javascript",
+        langFileName: "js"
+    },
+    {
+        langName: "java",
+        langFileName: "java"
+    },
+    {
+        langName: "python",
+        langFileName: "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.langName);
+} )
---
+++ 05_iterations/four.js
@@ -0,0 +1,32 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift by Apple'
+}
+
+for (const key in myObject) {
+    // console.log(key);
+    // console.log(myObject[key]);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(key);
+    // console.log(programming[key]);
+
+}
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+
+// for (const key in map) {
+//     console.log(key);
+        
+// }
